2f07f0f6f5b14da79d5fdcd35ffb642f718c5d4c
Steve Tice <stic6021@yahoo.com>
2019-11-01 16:12:59 +0000

    Lesson 19.

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,22 +1,36 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_user
+
   def create
-    @section = Section.find(params[:section_id])
-    @lesson = @section.lessons.create(lesson_params)
+    @lesson = current_section.lessons.create(lesson_params)
     if @lesson.valid?
-      redirect_to instructor_course_path(@section.course)
+      redirect_to instructor_course_path(current_section.course)
     else
       render :new, :unprocessable_entity
     end
   end
 
   def new
-    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
 
   private
 
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
+
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
+  end
+
+  def render_eperm
+    render plain: "Unauthorized", status: :Unauthorized
+  end
+
+  def require_authorized_user
+    return render_eperm unless current_section.course.user == current_user
   end
 end

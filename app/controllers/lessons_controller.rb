74a262283fb3b8499b742d7997f9666b224def97
Steve Tice <stic6021@yahoo.com>
2019-11-20 15:31:39 +0000

    Lesson 28.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_user
+
   def show
   end
 
   private
 
+  helper_method :current_course
+  def current_course
+    @current_course ||= current_lesson.section.course rescue nil
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_user
+    redirect_to(course_path(current_course), alert: "You must enroll in this course to view its lesson details.") unless current_user.enrolled_in?(current_course)
   end
 end

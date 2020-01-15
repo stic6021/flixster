b80b8ed90160b65324d129f433aea74a73468f9d
Steve Tice <stic6021@yahoo.com>
2020-01-15 13:02:06 +0000

    Fixed routing bug introduced with Lesson 35.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:create]
     end
   end
 end
